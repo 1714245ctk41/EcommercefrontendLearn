51880580fff5924ff9fca55039749f16dea304bb
Trần Văn Hoàng <1714245ctk41@gmail.com>
2020-06-04 15:05:54 +0700

    make better

---
+++ build/precache-manifest.bf2ebc35b2ffbdab61287b5c2e92e35e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90c45f4deda29b851fb5e7077fdc6300",
+    "url": "/EcommercefrontendLearn/index.html"
+  },
+  {
+    "revision": "f5d0a3c3783ad30ca724",
+    "url": "/EcommercefrontendLearn/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "49349e9131dbef93f8f1",
+    "url": "/EcommercefrontendLearn/static/js/2.9c816779.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EcommercefrontendLearn/static/js/2.9c816779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5d0a3c3783ad30ca724",
+    "url": "/EcommercefrontendLearn/static/js/main.fa0e0c64.chunk.js"
+  },
+  {
+    "revision": "af6108aa2e39a1965c39",
+    "url": "/EcommercefrontendLearn/static/js/runtime-main.4ce8c1b9.js"
+  }
+]);
